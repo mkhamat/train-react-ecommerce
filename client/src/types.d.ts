7c270e30174ddfc50271351f3563b7e7e239a41f
mkhamat <myxa@xakep.org>
2020-11-18 12:52:53 +0300

    types

---
+++ client/src/types.d.ts
@@ -0,0 +1,38 @@
+import { Action } from "redux";
+import { Product } from "./components/ProductList";
+
+export type Product = {
+  _id: string;
+  name: string;
+  description: string;
+  image: string;
+  price: number;
+  inStock: number;
+  rating: number;
+  reviews: [];
+  numberOfReviews: number;
+};
+
+export type CartPayload = {
+  product: Product;
+  qty: number;
+};
+export type CartItem = {
+  type: string;
+  payload?: CartPayload;
+};
+
+export type CartQty = {
+  type: string;
+  payload?: { id: string; qty: number };
+};
+
+export type ProductList = {
+  type: string;
+  payload?: Product[];
+};
+
+export type ProductList = {
+  type: string;
+  payload: Product;
+};
