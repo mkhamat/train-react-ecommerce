63f6c7ee3c0da12b8e3fef175ed60458c805d918
mkhamat <myxa@xakep.org>
2020-11-18 12:51:42 +0300

    added cart reducer

--- client/src/store.ts
+++ client/src/store.ts
@@ -3,10 +3,12 @@ import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import productListReducer from "./ducks/productList";
 import productReducer from "./ducks/product";
+import cartReducer from "./ducks/cart";
 
 const rootReducer = combineReducers({
   productList: productListReducer,
   product: productReducer,
+  cart: cartReducer,
 });
 const initialState = {};
 
