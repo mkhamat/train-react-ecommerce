48ca9b5bb5bfc2509fdd1e103517710f13df24d1
mkhamat <myxa@xakep.org>
2020-11-18 12:55:35 +0300

    remade navigation bar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,21 +1,49 @@
-import React, { useEffect } from "react";
+import MenuItem from "antd/lib/menu/MenuItem";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { Badge, Menu } from "antd";
+import { ShoppingCartOutlined, UserOutlined } from "@ant-design/icons";
+import { RootState } from "../store";
+import { useSelector } from "react-redux";
+import { Product } from "../types";
 
-export default function NavBar() {
+export default function Navbar() {
+  const cartItems: { cart: Product[] } = useSelector(
+    (state: RootState) => state.cart
+  );
+
   return (
-    <nav>
-        <div className="logo">
-          <h2>
-            <a href="#">ONSHOP</a>
-          </h2>
-        </div>
-        <ul className="nav-items">
-          <li>
-            <a href="">Cart</a>
-          </li>
-          <li>
-            <a href="">Sign In</a>
-          </li>
-        </ul>
-    </nav>
+    <Menu
+      selectedKeys={[""]}
+      style={{ maxWidth: "1200px", margin: "0 auto" }}
+      mode="horizontal"
+      theme="dark"
+    >
+      <Link to="/">
+        <span
+          style={{
+            color: "rgba(255, 255, 255, 0.65)",
+            fontWeight: "bold",
+          }}
+        >
+          ONSHOP
+        </span>
+      </Link>
+      <Menu.Item style={{ float: "right" }} icon={<ShoppingCartOutlined />}>
+        <Link to="/cart">
+          <Badge
+            count={cartItems.cart.length}
+            offset={[10, 0]}
+            size="small"
+            style={{ backgroundColor: "#222" }}
+          >
+            Cart
+          </Badge>
+        </Link>
+      </Menu.Item>
+      <MenuItem style={{ float: "right" }} icon={<UserOutlined />}>
+        <Link to="/login">Sign In</Link>
+      </MenuItem>
+    </Menu>
   );
 }
