4bc73f018bdb415d6851c5b4d95df7b666ef8508
mkhamat <myxa@xakep.org>
2020-11-19 12:06:46 +0300

    created rating component with antd

--- client/src/components/Rating.tsx
+++ client/src/components/Rating.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import {BsStar, BsStarFill} from "react-icons/bs";
+import { Rate } from "antd";
+import React from "react";
+import productListReducer from "../ducks/productList";
 
-export default function Rating () {
-    return (
-            <div className="procut-reviews">
-                <BsStar/>
-            </div>
-    )
+export default function Rating({
+  rating,
+  num,
+  disabled,
+}: {
+  rating: number;
+  num: number;
+  disabled: boolean;
+}) {
+  return (
+    <div>
+      <Rate disabled={disabled} defaultValue={rating} allowHalf />
+      {rating ? (
+        <p>
+          {rating} of 5 based on {num} reviews
+        </p>
+      ) : (
+        <p>No reviews</p>
+      )}
+    </div>
+  );
 }
