07dc71bbfa0cbf6922968899dbfcc278fd2b9d5b
Mkhamat <myxa@xakep.org>
2020-11-25 19:29:42 +0300

    basic routes for products

--- backend/routes/productsRoutes.ts
+++ backend/routes/productsRoutes.ts
@@ -1,1 +1,9 @@
-import { router } from "express";
+import * as express from "express";
+import { fetchProducts } from "../controllers/productsController";
+const router = express.Router();
+
+router.get("/", fetchProducts);
+
+router.get("/:id", fetchProducts);
+
+export { router };
