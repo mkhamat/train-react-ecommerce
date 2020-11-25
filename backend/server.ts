f40249516767d5f24e0f469d2dd07f42b828e051
Mkhamat <myxa@xakep.org>
2020-11-25 19:24:19 +0300

    feature: user authentication

--- backend/server.ts
+++ backend/server.ts
@@ -1,7 +1,9 @@
 import * as express from "express";
 import * as mongoose from "mongoose";
 import * as dotenv from "dotenv";
-import Product from "./models/Product";
+import * as bodyParser from "body-parser";
+import { router as products } from "./routes/productsRoutes";
+import { router as users } from "./routes/userRoutes";
 
 dotenv.config();
 
@@ -11,6 +13,10 @@ app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
 });
 
+app.use(bodyParser.json());
+app.use("/products", products);
+app.use("/users", users);
+
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
@@ -19,17 +25,3 @@ mongoose
   })
   .then((res) => console.log("Mongo is running on " + res.connection.port))
   .catch((error) => console.log(error));
-
-app.get("/products", (req, res) => {
-  Product.find()
-    .then((data) => res.json(data))
-    .catch((err) => console.log(err));
-});
-
-app.get("/products/:id", (req, res) => {
-  Product.findById(req.params.id)
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((err) => console.log(err));
-});
