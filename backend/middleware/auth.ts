c67a152640a030dcc95bf961e12b4bd1649ff8d8
Mkhamat <myxa@xakep.org>
2020-11-25 19:27:01 +0300

    gaining jwtoken from client and verifying

---
+++ backend/middleware/auth.ts
@@ -0,0 +1,16 @@
+import * as jwt from "jsonwebtoken";
+import User from "../models/User";
+
+export async function authorize(req, res, next) {
+  let token =
+    req.headers.authorization.startsWith("Bearer") &&
+    req.headers.authorization.split(" ")[1];
+  let decoded = token && (await jwt.verify(token, process.env.JWT_SECRET));
+  if (decoded) {
+    req.user = await User.findById(decoded.id).select("-password");
+    next();
+  } else {
+    res.status(401);
+    res.send("Token is invalid");
+  }
+}
