328f2b6f87742d8f964cb1ec3433c2d662be6ac1
Mkhamat <myxa@xakep.org>
2020-12-13 02:01:08 +0300

    add user reducer and get data from local storage

--- client/src/store.ts
+++ client/src/store.ts
@@ -4,13 +4,25 @@ import { composeWithDevTools } from "redux-devtools-extension";
 import productListReducer from "./ducks/productList";
 import productReducer from "./ducks/product";
 import cartReducer from "./ducks/cart";
+import userReducer from "./ducks/user";
 
 const rootReducer = combineReducers({
   productList: productListReducer,
   product: productReducer,
   cart: cartReducer,
+  users: userReducer,
 });
-const initialState = {};
+
+let userInfoFromLS = localStorage.getItem("userInfo");
+if (userInfoFromLS) {
+  userInfoFromLS = JSON.parse(userInfoFromLS);
+} else {
+  userInfoFromLS = null;
+}
+
+const initialState: any = {
+  users: { user: userInfoFromLS },
+};
 
 export default createStore(
   rootReducer,
