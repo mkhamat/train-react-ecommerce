340b45e80cbbd370d235970d926f963dcf260a7d
Mkhamat <myxa@xakep.org>
2020-12-13 02:02:11 +0300

    show user name in navbar when logged in

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,17 +1,33 @@
-import MenuItem from "antd/lib/menu/MenuItem";
 import React from "react";
-import { Link } from "react-router-dom";
-import { Badge, Menu } from "antd";
-import { ShoppingCartOutlined, UserOutlined } from "@ant-design/icons";
+import { Link, useHistory } from "react-router-dom";
+import { Badge, Menu, message } from "antd";
+import {
+  LogoutOutlined,
+  ProfileTwoTone,
+  ShoppingCartOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
 import { RootState } from "../store";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Product } from "../types";
+import SubMenu from "antd/lib/menu/SubMenu";
+import { logout } from "../ducks/user";
 
 export default function Navbar() {
+  const dispatch = useDispatch();
+  const history = useHistory();
   const cartItems: { cart: Product[] } = useSelector(
     (state: RootState) => state.cart
   );
 
+  const { user }: { user: { name: string } } = useSelector(
+    (state: RootState) => state.users
+  );
+  function handleLogout() {
+    dispatch(logout());
+    message.info("You logged out");
+    history.push("/");
+  }
   return (
     <Menu
       selectedKeys={[""]}
@@ -20,16 +36,16 @@ export default function Navbar() {
       theme="dark"
     >
       <Menu.Item>
-      <Link to="/">
-        <span
-          style={{
-            color: "rgba(255, 255, 255, 0.65)",
-            fontWeight: "bold",
-          }}
-        >
-          ONSHOP
-        </span>
-      </Link>
+        <Link to="/">
+          <span
+            style={{
+              color: "rgba(255, 255, 255, 0.65)",
+              fontWeight: "bold",
+            }}
+          >
+            ONSHOP
+          </span>
+        </Link>
       </Menu.Item>
       <Menu.Item style={{ float: "right" }} icon={<ShoppingCartOutlined />}>
         <Link to="/cart">
@@ -43,9 +59,24 @@ export default function Navbar() {
           </Badge>
         </Link>
       </Menu.Item>
-      <MenuItem style={{ float: "right" }} icon={<UserOutlined />}>
-        <Link to="/login">Sign In</Link>
-      </MenuItem>
+      {user ? (
+        <SubMenu
+          style={{ float: "right" }}
+          icon={<UserOutlined />}
+          title={user.name}
+        >
+          <Menu.Item icon={<ProfileTwoTone />}>
+            {<Link to={"/users/profile"}>Profile</Link>}
+          </Menu.Item>
+          <Menu.Item onClick={handleLogout} icon={<LogoutOutlined />}>
+            Log Out
+          </Menu.Item>
+        </SubMenu>
+      ) : (
+        <Menu.Item style={{ float: "right" }} icon={<UserOutlined />}>
+          <Link to="/users/login">Sign In</Link>
+        </Menu.Item>
+      )}
     </Menu>
   );
 }
