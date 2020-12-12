a6116216075970904e7651c8df1fd68e93632902
Mkhamat <myxa@xakep.org>
2020-12-13 01:57:44 +0300

    add possibility to update user info

--- backend/controllers/usersController.ts
+++ backend/controllers/usersController.ts
@@ -1,55 +1,96 @@
-import User from "../models/User";
-import * as jwt from "jsonwebtoken";
+import User from "../models/User"
+import * as jwt from "jsonwebtoken"
 
-async function authenticate(req, res) {
-  let user: any = await User.findOne({ email: req.body.email });
-  if (user && (await user.matchPassword(req.body.password))) {
-    res.json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      isAdmin: user.isAdmin,
-      token: jwt.sign({ id: user._id }, process.env.JWT_SECRET),
-    });
-  } else {
-    res.status("401");
-    res.send("not authenticated");
-  }
-}
-
-async function sign(req, res) {
-  let userExists = await User.findOne({ email: req.body.email });
-  if (userExists) {
-    res.status(400).send("User exists");
-  } else {
-    let newUser: any = await User.create(req.body); //will change
-    if (newUser) {
-      res.status(201).json({
-        _id: newUser._id,
-        name: newUser.name,
-        email: newUser.email,
-        isAdmin: newUser.isAdmin,
-        token: jwt.sign({ id: newUser._id }, process.env.JWT_SECRET),
-      });
+const authenticate = async (req, res, next) => {
+  try {
+    let user: any = await User.findOne({ email: req.body.email })
+    if (user && (await user.matchPassword(req.body.password))) {
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        token: jwt.sign({ id: user._id }, process.env.JWT_SECRET),
+      })
     } else {
-      res.status(400).send("Invalid user data");
+      res.status(401)
+      throw new Error("Invalid email or password")
     }
+  } catch (error) {
+    next(error)
   }
 }
 
-async function showProfile(req, res) {
-  let user: typeof req.user = await User.findById(req.user._id);
-  if (user) {
-    res.json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      isAdmin: user.isAdmin,
-    });
-  } else {
-    res.status(404);
-    res.send("User not found");
+const sign = async (req, res, next) => {
+  try {
+    let userExists = await User.findOne({ email: req.body.email })
+    if (userExists) {
+      res.status(400).send("User exists")
+    } else {
+      let newUser: any = await User.create(req.body)
+      if (newUser) {
+        res.status(201).json({
+          _id: newUser._id,
+          name: newUser.name,
+          email: newUser.email,
+          isAdmin: newUser.isAdmin,
+          token: jwt.sign({ id: newUser._id }, process.env.JWT_SECRET),
+        })
+      } else {
+        res.status(400)
+        throw new Error("Invalid user data")
+      }
+    }
+  } catch (error) {
+    next(error)
   }
 }
 
-export { authenticate, showProfile, sign };
+const getProfile = async (req: any, res, next) => {
+  try {
+    let user: typeof req.user = await User.findById(req.user._id)
+    if (user) {
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+      })
+    } else {
+      res.status(404)
+      throw new Error("User not found")
+    }
+  } catch (error) {
+    next(error)
+  }
+}
+
+const updateProfile = async (req: any, res, next) => {
+  try {
+    let user: typeof req.user = await User.findById(req.user._id)
+    if (user) {
+      user.name = req.body.name || user.name
+      user.email = req.body.email || user.email
+      if (req.body.password) {
+        user.password = req.body.password
+      }
+      let updatedUser = await user.save()
+      if (updatedUser) {
+        res.json({
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+          isAdmin: user.isAdmin,
+          token: jwt.sign({ id: user._id }, process.env.JWT_SECRET),
+        })
+      }
+    } else {
+      res.status(404)
+      throw new Error("User not found")
+    }
+  } catch (error) {
+    next(error)
+  }
+}
+
+export { authenticate, getProfile, sign, updateProfile }
